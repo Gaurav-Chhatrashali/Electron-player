3cd21a758d52220fec184dc3863bf976d4da06e0
Gaurav Chhatrashali <gaurav.turner@gmail.com>
2019-03-29 18:42:30 +0530

    titlebar library add

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 if (process.platform == 'win32')
   process.env['VLC_PLUGIN_PATH'] = require('path').join(__dirname, 'node_modules/wcjs-prebuilt/bin/plugins');
 var electron = require("electron");
-const { app, BrowserWindow} = electron;
+const { app, BrowserWindow } = electron;
 var mainWindow = null;
 function createWindow() {
-   win = new BrowserWindow({width: 800, height: 600})
-   win.loadURL('file://' + __dirname + '/index.html')
-   win.openDevTools({ detach: false});
+  win = new BrowserWindow({ width: 800, height: 600, frame: false })
+  win.loadURL('file://' + __dirname + '/index.html')
+  win.openDevTools({ detach: false });
 }
 app.on('ready', createWindow)
